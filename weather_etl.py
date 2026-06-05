8b0de86ac351cd828711cd445d0af99f559ef4a8
Gokul Tharun <gokultharun03@gmail.com>
2026-06-05 20:04:52 +0530

    fully up and ready

--- weather_etl.py
+++ weather_etl.py
@@ -19,7 +19,6 @@ def extract_weather_data(location):
         "latitude": location["latitude"],
         "longitude": location["longitude"],
         "hourly": "temperature_2m,precipitation",
-        "daily": "sunshine_duration",
         "current":'precipitation,temperature_2m',
         'timezone': "auto",
         'past_days': 14,
@@ -34,11 +33,6 @@ def extract_weather_data(location):
         print(f"Error while fetching: {e}")
         return None
 
-
-def data_dump(raw_data):
-    with open('weather_data_raw.json', 'w') as f:
-        json.dump(raw_data, f, indent=4)
-
 def raw_data_cleaner(raw_data):
     try:
         latitude = raw_data['latitude']
@@ -47,7 +41,6 @@ def raw_data_cleaner(raw_data):
         hourwise = raw_data['hourly']['time']
         hourwise_temp2m = raw_data['hourly']['temperature_2m']
         hourwise_precipitation = raw_data['hourly']['precipitation']
-        sunshine_duration = raw_data['daily']['sunshine_duration']
         
         df = pd.DataFrame({
             'latitude': latitude,
@@ -57,22 +50,24 @@ def raw_data_cleaner(raw_data):
             'time': pd.to_datetime(hourwise).time,
             'temperature_2m': hourwise_temp2m,
             'precipitation': hourwise_precipitation,
-            'sunshine_duration': sunshine_duration
         })
-        df['sunshine_duration'] = df['sunshine_duration']/3600
-
         return df
 
     except KeyError as e:
         print(f"API structure changed, Missing key: {e}")
         return None
-# res = extract_weather_data(location={"latitude": 27.3257, "longitude": -88.6122})
-# print(res)
+
+
+def extract_weather(x):
+    location = get_location(x)
+    raw_data = extract_weather_data(location)
+    df = raw_data_cleaner(raw_data)
+    return df
+
+
 
 #x=input("Enter the location: ")
-
-#location = get_location(x)
-#raw_data = extract_weather_data(location)
+# print(df.head())
 #data_dump(raw_data)
 
 # if location:
@@ -80,9 +75,9 @@ def raw_data_cleaner(raw_data):
 #     if weather_data:
 #         print(weather_data)
 
-with open('Tour_etl_pipeline/weather_data_raw.json', 'r') as f:
-    raw_data = json.load(f)
+# with open('Tour_etl_pipeline/weather_data_raw.json', 'r') as f:
+#     raw_data = json.load(f)
 
-df = raw_data_cleaner(raw_data)
-print(df.head())
-
+# def data_dump(raw_data):
+#     with open('weather_data_raw.json', 'w') as f:
+#         json.dump(raw_data, f, indent=4)
