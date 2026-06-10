e667230d7c62453017607a9e325c409f2c1ec246
Gokul Tharun <gokultharun03@gmail.com>
2026-06-10 17:48:15 +0530

    scheduled pipeline every week

---
+++ pipeline_3.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d D:\Coding\Projects\Tour_etl_pipeline
+call venv\Scripts\activate
+python pipeline_3.py
--- sql/queries.sql
+++ sql/queries.sql
@@ -12,13 +12,15 @@ select * from monthly_weather;
 
 select * from best_time;
 
+select * from hist_flight;
+
 describe monthly_weather;
 
-alter table best_time
-add column fetched_at datetime;
+alter table hist_flight
+add column location varchar(50);
 
-alter table monthly_weather
-modify location varchar(35) NULL;
+alter table hist_flight
+modify price decimal(10,1);
 
 alter table hotel
 rename column airport to close_to_airport;
@@ -33,3 +35,4 @@ truncate table monthly_flight;
 truncate table monthly_hotel;
 truncate table monthly_weather;
 truncate table best_time;
+truncate table hist_flight;
